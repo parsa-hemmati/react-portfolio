0ebeaf91da3976bd9eedf8246f9e556e1141e908
Parsa Hemmati <parsa.hemmati@gmail.com>
2023-03-14 15:35:48 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation';
+// import Contact from './Contact';
+
+function Header() {
+  return (
+    <header>
+      <Navigation />
+      {/* <Contact /> */}
+    </header>
+  );
+}
+
+export default Header;
